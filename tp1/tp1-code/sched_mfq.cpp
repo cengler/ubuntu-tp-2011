481d4a46f9b2e45709708a1efa15c106eab4d1ed
caeycae@gmail.com <caeycae@gmail.com@c8c99a00-93c8-7013-be0c-202701465c0b>
2011-04-07 00:31:29 +0000

    el algoritmo de MFQ

--- tp1/tp1-code/sched_mfq.cpp
+++ tp1/tp1-code/sched_mfq.cpp
@@ -3,22 +3,33 @@
 #include "sched_mfq.h"
 #include "basesched.h"
 
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
 SchedMFQ::SchedMFQ(vector<int> argn) {
-	quantums = argn;
+	quantums.swap(argn);
 	qlength = quantums.size();
+	qs = vector<queue<int> >(qlength);
+	// CREO LAS qlength COLAS
+	for(int i=0;i<qlength;i++) {
+		queue<int> q;
+		qs[i]=q;
+	}
 }
 
 void SchedMFQ::load(int pid) {
-	p[pid] = qlength-1;
-	(qs[qlength-1]).push(pid);
+	// LE SETEO PRIORIDAD MAXIMA Y LO ENCOLO A LA COLA DE MAXIMA PRIORIDAD
+	p[pid] = MAX_PRIORITY;
+	(qs[MAX_PRIORITY]).push(pid);
 }
 
 void SchedMFQ::unblock(int pid) {
-	if( p[pid] == 0 )
-		(qs[0]).push(pid);
-	else {
+	// SI TIENE LA PIORIDAD MAS ALTA LA DEJO EN ESA COLA
+	if( p[pid] == MAX_PRIORITY )
+		(qs[MAX_PRIORITY]).push(pid);
+	else { // LE SUBIMOS LA PRIORIDAD
 		p[pid] = p[pid]-1;
 		(qs[p[pid]]).push(pid);
 	}
@@ -34,31 +45,57 @@ int SchedMFQ::firstNotEmptyQueue() {
 }
 
 int SchedMFQ::tick(const enum Motivo m) {
-	
+
 	int q_index = firstNotEmptyQueue();
 	
-	if (q_index != -1) 
-			return IDLE_TASK;
-	
 	// SI EL PROCESO TERMINO O SE BLOQUEO	
-	if (m == EXIT || m == BLOCK) {
-		// TOMO EL SIG A EJECUTAR CON QUOTA EN 0
-		quota = 0;
-		int sig = qs[q_index].front(); qs[q_index].pop();
-		return sig;
+	if (m == EXIT) {
+		if (q_index == -1) 
+			return IDLE_TASK;
+		else {
+			quota = 0;
+			int sig = qs[q_index].front(); qs[q_index].pop();
+			return sig;
+		}
+	}
+			
+	if(m == BLOCK) {
+		if (q_index == -1) 
+			return current_pid();
+		else {
+			// TOMO EL SIG A EJECUTAR CON QUOTA EN 0
+			quota = 0;
+			int sig = qs[q_index].front(); qs[q_index].pop();
+			return sig;
+		}
 	} 
+	
+	
+	if(current_pid() == IDLE_TASK) {
+		if (q_index == -1) 
+			return IDLE_TASK;
+		else {
+			quota = 0;
+			int sig = qs[q_index].front(); qs[q_index].pop();
+			return sig;
+		}
+	}
 	
 	int currentPiority = p[current_pid()];
 	int currentQuantum = quantums[currentPiority];
+	//printf("currentPiority %d\n", currentPiority);
+	//printf("currentQuantum %d\n", currentQuantum);
 	// SE CUMPLIO LA CUOTA
-	if(quota >= currentQuantum) {
+	if(quota >= currentQuantum-1) {
 		
 		// SI NO TIENE MINIMA PIORIDAD SE LA BAJO
 		if( currentPiority != qlength-1 ) { 
-			currentPiority--;
+			currentPiority++;
+			printf("AUMENTO\n");
 		}
 		// AGREGO EL PROCESO EXPULSADO A LA COLA CON
 		// LA PIORIDAD ACTUALIZADA
+		p[current_pid()] = currentPiority;
 		qs[currentPiority].push(current_pid());
 		// LANZO EL SIG PROCESO CON QUOTA EN 0
 		quota = 0;
@@ -69,5 +106,4 @@ int SchedMFQ::tick(const enum Motivo m) {
 		quota++;
 		return current_pid();	
 	}
-	
 }
