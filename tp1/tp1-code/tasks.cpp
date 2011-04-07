47286a64de9eaa618d00613ff2ae13efd6704bb0
caeycae@gmail.com <caeycae@gmail.com@c8c99a00-93c8-7013-be0c-202701465c0b>
2011-04-07 13:06:54 +0000

    Agrego tipos de tareas y un buen test para MFQ

--- tp1/tp1-code/tasks.cpp
+++ tp1/tp1-code/tasks.cpp
@@ -12,7 +12,7 @@ void TaskIO(vector<int> params) { // params: ms_cpu, ms_io,
 	uso_IO(params[1]); // Uso IO ms_io milisegundos.
 }
 
-void TaskCon(vector<int> params) { // params: n
+void TaskCon(vector<int> params) { // params: 3
 	int n = params[0];
 	int bmin = params[1];
 	int bmax = params[2];
@@ -24,6 +24,32 @@ void TaskCon(vector<int> params) { // params: n
 	}
 }
 
+void TaskLongCPUMultipleIO(vector<int> params) { // params: n
+	uso_CPU(params[0]); // Uso el CPU ms_cpu milisegundos.
+	int n = params[1];
+	int bmin = params[2];
+	int bmax = params[3];
+	for( int i = 0; i < n ; i++ ) {
+		int time = bmin;
+		if(bmin != bmax)
+			time = (rand()%(bmax-bmin))+bmin;
+		uso_IO(time);
+	}
+}
+
+void TaskMultipleIOLongCPU(vector<int> params) { // params: n
+	int n = params[1];
+	int bmin = params[2];
+	int bmax = params[3];
+	for( int i = 0; i < n ; i++ ) {
+		int time = bmin;
+		if(bmin != bmax)
+			time = (rand()%(bmax-bmin))+bmin;
+		uso_IO(time);
+	}
+	uso_CPU(params[0]); // Uso el CPU ms_cpu milisegundos.
+}
+
 void tasks_init(void) {
 	/* Todos los tipos de tareas se deben registrar acá para poder ser usadas.
 	 * El segundo parámetro indica la cantidad de parámetros que recibe la tarea
@@ -31,5 +57,7 @@ void tasks_init(void) {
 	register_task(TaskCPU, 1);
 	register_task(TaskIO, 2);
 	register_task(TaskCon, 3);
+	register_task(TaskLongCPUMultipleIO, 4);
+	register_task(TaskMultipleIOLongCPU, 4);
 
 }
